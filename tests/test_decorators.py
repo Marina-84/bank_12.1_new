1973881fbc548f0eba9fac7464e4baa9a829afcf
Marina <lav_mari@mail.ru>
2024-09-28 23:03:03 +0300

    fixed?

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -2,19 +2,25 @@ from typing import Any
 
 import pytest
 
-from src.decorators import log
-
-
-def test_log(capsys) -> Any:
-    with pytest.raises(Exception):
-        captured = capsys.readouterr()
-        assert captured.out == "my_function error\n"
-    my_decorator(2, 5)
+from src.decorators import log, my_function
 
 
 @log(filename="mylog.txt")
-def my_decorator(x: int, y: int) -> Any:
-    return x + y
+def test_log(capsys) -> Any:
+    with pytest.raises(Exception):
+        captured = capsys.readouterr()
+        assert captured.out == Exception
+        my_function(x=[3], y=[1, 2, 3])
 
-def my_decorator(x="6", y=[1, 2, 3]) -> Any:
-    return x + y
+
+def test_my_function(x=1, y=2) -> Any:
+    result = x + y
+    try:
+        assert result == "5"
+    except AssertionError:
+        print("Error")
+
+
+def test_my_function_2(x=1, y=2) -> Any:
+    result = x + y
+    assert result == 3
