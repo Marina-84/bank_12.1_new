598410a3fc973d388bb52e99f488f34b678ecd35
Marina <lav_mari@mail.ru>
2024-09-30 05:54:02 +0300

    next

--- src/decorators.py
+++ src/decorators.py
@@ -1,7 +1,7 @@
 from typing import Any, Callable, Optional
 
 
-def log(filename: Optional[str]) -> Callable:
+def log(filename: Optional[str] = "test.log") -> Callable:
     """Декоратор создает логирование в функции."""
 
     def my_decorator(func) -> Any:
@@ -20,16 +20,14 @@ def log(filename: Optional[str]) -> Callable:
                         file.write(f"{func.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}")
                 else:
                     print(f"{func.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}")
-
         return wrapper
-
     return my_decorator
 
 
-@log(filename="mylog.txt")
+@log(filename="test.log")
 def my_function(x: Any, y: Any) -> Any:
     """Функция складывает два значения"""
     return x + y
 
 
-my_function(1,2)
+my_function(1, 2)
