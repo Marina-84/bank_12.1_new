c51f3c25e4b06ef7b84283c1175edd6661a6ed3c
Marina <lav_mari@mail.ru>
2024-10-09 00:15:45 +0300

    homework done

--- src/external_api.py
+++ src/external_api.py
@@ -1,4 +1,6 @@
 import os
+from typing import Optional
+
 import requests
 from dotenv import load_dotenv
 
@@ -6,39 +8,33 @@ load_dotenv()
 apikey = os.getenv("API_KEY")
 
 
-def current_conversion(transactions: dict):
+def current_conversion(transactions: dict) -> Optional[float]:
     curr_to = "RUB"
     curr_from = transactions["operationAmount"]["currency"]["code"]
     amount = transactions["operationAmount"]["amount"]
     url = f"https://api.apilayer.com/exchangerates_data/convert?to={curr_to}&from={curr_from}&amount={amount}"
     payload: dict = {}
-    headers = {"apikey": "nKc8pYSwLYU0siyO6F8iFw2bnLhAg0Z8"}
+    headers = {"apikey": apikey}
     response = requests.get(url, headers=headers, data=payload)
-    status_code = response.status_code
+    status_code = response.status_codegit
     if status_code != 200:
         print(response.reason)
     elif curr_from == "RUB":
-        return amount
+        return float(amount)
     else:
         result = response.json()
         data = result.get("result")
-        return data
+        return float(data)
 
 
 transactions = {
     "id": 863064926,
     "state": "EXECUTED",
     "date": "2019-12-08T22:46:21.935582",
-    "operationAmount": {
-      "amount": "41096.24",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
+    "operationAmount": {"amount": "41096.24", "currency": {"name": "USD", "code": "USD"}},
     "description": "Открытие вклада",
-    "to": "Счет 90424923579946435907"
-  }
+    "to": "Счет 90424923579946435907",
+}
 
 if __name__ == "__main__":
     current_conversion(transactions)
--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,21 +1,27 @@
 from unittest.mock import patch
-
 import pytest
 import requests
 
 from src.external_api import current_conversion
 
 transactions = {
-    "id": 863064926,
+    "id": 596171168,
     "state": "EXECUTED",
-    "date": "2019-12-08T22:46:21.935582",
-    "operationAmount": {"amount": "41096.24", "currency": {"name": "USD", "code": "USD"}},
+    "date": "2018-07-11T02:26:18.671407",
+    "operationAmount": {
+      "amount": "79931.03",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
     "description": "Открытие вклада",
-    "to": "Счет 90424923579946435907",
-}
+    "to": "Счет 72082042523231456215"
+  }
 
 
 @patch("requests.get")
 def test_current_conversion(mock_get):
-    mock_get.return_value.json.return_value = transactions
-    assert current_conversion(transactions) == None
+    mock_get.return_value.json.return_value = {"result": 79931.03}
+    mock_get.return_value.status_code = 200
+    assert current_conversion(transactions) == 79931.03
