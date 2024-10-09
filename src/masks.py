f2724a4fe40d7daf870ee9049959ac94c7be5183
Marina <lav_mari@mail.ru>
2024-10-10 00:26:45 +0300

    homework done

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,41 @@
+import logging
+import os
+
+# Получаем абсолютный путь до текущей директории
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Создаем путь до файла логов относительно текущей директории
+rel_file_path = os.path.join(current_dir, "../log/masks.log")
+abs_file_path = os.path.abspath(rel_file_path)
+
+# Добавляем логгер, который записывает логи в файл.
+logger = logging.getLogger("masks")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(abs_file_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_num: str) -> str:
     """Функция возвращает маску номера карты"""
-    return f"{card_num[:4]} {card_num[4:6]}** **** {card_num[12:]}"
+    if len(card_num) == 16:
+        logger.info("Формат карты верный")
+        return f"{card_num[:4]} {card_num[4:6]}** **** {card_num[12:]}"
+    else:
+        logger.warning("Неверный формат банковской карты")
+        return "Неверный формат банковской карты"
 
 
 def get_mask_account(acc_num: str) -> str:
     """Функция возвращает маску номера счета"""
-    return f"**{acc_num[-4:]}"
+    if len(acc_num) == 20:
+        logger.info("Формат счета верный")
+        return f"**{acc_num[-4:]}"
+    else:
+        logger.warning("Неверный формат счета")
+        return "Неверный формат номера счета"
+
+
+if __name__ == "__main__":
+    get_mask_card_number("3456789043215678")
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,25 @@
 import json
+import logging
+import os
+
+# Получаем абсолютный путь до текущей директории
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Создаем путь до файла логов относительно текущей директории
+rel_log_file_path = os.path.join(current_dir, "../log/utils.log")
+abs_log_file_path = os.path.abspath(rel_log_file_path)
+
+# Создаем путь до файла JSON относительно текущей директории
+rel_src_file_path = os.path.join(current_dir, "../data/operations.json")
+abs_src_file_path = os.path.abspath(rel_src_file_path)
+
+# Добавляем логгер, который записывает логи в файл.
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(abs_log_file_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_transactions_list(path: str) -> list[dict]:
@@ -7,12 +28,13 @@ def get_transactions_list(path: str) -> list[dict]:
     try:
         with open(path, encoding="utf-8") as f:
             try:
+                logger.info("Путь до файла json верный")
                 transactions_list = json.load(f)
             except json.JSONDecodeError:
-                print("Введены некорректные данные")
+                logger.warning("Введены некорректные данные")
                 return error_list
     except FileNotFoundError:
-        print("Файл не найден")
+        logger.warning("Файл не найден")
         return error_list
 
     return transactions_list
